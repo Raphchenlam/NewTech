5f2ecb7d7c6b16a4b5526b2ef3d8896c26c9519a
Raphael Chenard-Lamothe <200633170@cegepsherbrooke.qc.ca>
2023-08-24 12:25:46 -0400

    ansible second commit

--- ansible.sh
+++ ansible.sh
@@ -3,4 +3,7 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install software-properties-common ansible -y
+sudo apt install software-properties-common ansible expect -y
+sudo apt auto-remove -y
+
+ssh-keygen -t rsa -C "200633170@cegepsherbrooke.qc.ca" -N "" -f ~/.ssh/id_rsa
